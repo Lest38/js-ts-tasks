c4f36b5f8ed62553f9f53c5854b17b6d85129a1a
Alina <alinakomar10@gmail.com>
2024-11-26 00:23:18 +0200

    feat(SDC-04): add solution task completed # Conflicts: #	src/backToFront.js #	src/compareSalary.js #	src/compareTwoNumbers.js #	src/digitsSum.js #	src/divider.js #	src/evalExpr.js #	src/sumInRange.js #	src/sumTwoNumbers.js #	src/volumeCone.js

--- src/sort.js
+++ src/sort.js
@@ -10,21 +10,6 @@
  */
 module.exports.sort = function sort(TestUtils) {
   return function (...args) {
-    if (!Array.isArray(args)) {
-      throw new TypeError('Input must be an array.');
-    }
-
-    const array = [...args]; // Copy the input to avoid mutating the original
-
-    // Bubble Sort using the comparator
-    for (let i = 0; i < array.length; i++) {
-      for (let j = 0; j < array.length - 1; j++) {
-        if (TestUtils.sortComparator(array[j + 1], array[j])) {
-          [array[j], array[j + 1]] = [array[j + 1], array[j]];
-        }
-      }
-    }
-
-    return array;
+    return args.sort(TestUtils.sortComparator);
   };
 };
