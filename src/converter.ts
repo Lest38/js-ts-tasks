d31143386564080773eaf247ea465994d2ca14ca
Alina Komar <132215478+Lest38@users.noreply.github.com>
2025-02-07 18:29:02 +0200

    Update converter.ts

--- src/converter.ts
+++ src/converter.ts
@@ -3,7 +3,7 @@
  * @param {string | number} value
  * @param {'m'|'mi'|'gr'|'pound'|'C'|'K'} from
  * @param {'m'|'mi'|'gr'|'pound'|'C'|'K'} to
- * @returns {number}
+ * @return{number}
  */
 module.exports.converter = function (value: number, from: string, to: string): number {
   throw new Error('Not implemented'); // delete this line and write your code
